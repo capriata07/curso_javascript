ce64cc9e3a31952971fedab8f2dd8c56fc25c7e4
Rafael Capriata de Souza Azevedo <rafael.azevedo@economia.gov.br>
2023-10-02 16:53:16 -0400

    feat: Testando && e || no Javascript e ===

---
+++ aula03.js
@@ -0,0 +1,8 @@
+const logo = 'brasil'
+let idade = 20
+
+if(idade <= 17 || idade === '20'){
+    console.log('Bem Vindo ao Brasil Beach ')
+}else{
+    console.log('Seu nome nao esta na lista de convidado, \n Aguarde a proxima chamada.!!')
+}
